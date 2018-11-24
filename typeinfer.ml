3a6e9250df9ae8d867bb8ac16ca8c79ef745aa6e
MaiconCunhaVieira <maicon_c_v@hotmail.com>
2018-11-24 16:42:26 -0200

    bug in typeinfer fixed

--- typeinfer.ml
+++ typeinfer.ml
@@ -12,19 +12,19 @@ let rec get_constraints (envmnt : env) (e : expr) = (
 		| Binop(op, e1, e2) -> ( match(op) with
 			  Sum -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
 						let (typeE2, constraintE2) = get_constraints envmnt e2 in
-							let newConstraint = [(typeE1, TyInt), (typeE2, TyInt)] in
+							let newConstraint = [(typeE1, TyInt); (typeE2, TyInt)] in
 								(TyInt, List.concat[newConstraint; constraintE1; constraintE2])
 			| Sub -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
 						let (typeE2, constraintE2) = get_constraints envmnt e2 in
-							let newConstraint = [(typeE1, TyInt), (typeE2, TyInt)] in
+							let newConstraint = [(typeE1, TyInt); (typeE2, TyInt)] in
 								(TyInt, List.concat[newConstraint; constraintE1; constraintE2])
 			| Mult -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
 						let (typeE2, constraintE2) = get_constraints envmnt e2 in
-							let newConstraint = [(typeE1, TyInt), (typeE2, TyInt)] in
+							let newConstraint = [(typeE1, TyInt); (typeE2, TyInt)] in
 								(TyInt, List.concat[newConstraint; constraintE1; constraintE2])
 			| Div -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
 						let (typeE2, constraintE2) = get_constraints envmnt e2 in
-							let newConstraint = [(typeE1, TyInt), (typeE2, TyInt)] in
+							let newConstraint = [(typeE1, TyInt); (typeE2, TyInt)] in
 								(TyInt, List.concat[newConstraint; constraintE1; constraintE2])
 		)
 		| Unop(op, e1) -> ( match(op) with
@@ -38,7 +38,7 @@ let rec get_constraints (envmnt : env) (e : expr) = (
 		| If(e1, e2, e3) -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
 								let (typeE2, constraintE2) = get_constraints envmnt e2 in
 									let (typeE3, constraintE3) = get_constraints envmnt e3 in
-										let newConstraint = [(typeE1, TyBool), (typeE2, typeE3)] in
+										let newConstraint = [(typeE1, TyBool); (typeE2, typeE3)] in
 											(typeE2, List.concat[newConstraint; constraintE1; constraintE2; constraintE3])
 		| Try(e1, e2) -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
 							let (typeE2, constraintE2) = get_constraints envmnt e2 in
