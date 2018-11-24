fb30293ca3910f8c9fb68bf7b9547030eb829c20
MaiconCunhaVieira <maicon_c_v@hotmail.com>
2018-11-24 21:46:44 -0200

    app, nil, cons, hd, tl, isempty and raise rules added

--- typeinfer.ml
+++ typeinfer.ml
@@ -75,6 +75,29 @@ let rec get_constraints (envmnt : env) (e : expr) = (
 									let (typeE3, constraintE3) = get_constraints envmnt e3 in
 										let newConstraint = [(typeE1, TyBool); (typeE2, typeE3)] in
 											(typeE2, List.concat[newConstraint; constraintE1; constraintE2; constraintE3])
+		| App(e1, e2) -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
+							let (typeE2, constraintE2) = get_constraints envmnt e2 in
+								let newType = newVar count in
+									let newConstraint = [(typeE1, TyFn(typeE2, TyId(newType)))] in
+										(TyId(newType), List.concat[newConstraint; constraintE1; constraintE2])
+		| Nil -> let newType = newVar count in (TyList(TyId(newType)), [])
+		| Cons(e1, e2) -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
+							let (typeE2, constraintE2) = get_constraints envmnt e2 in
+								let newConstraint = [(TyList(typeE1), typeE2)] in
+									(typeE2, List.concat[newConstraint; constraintE1; constraintE2])
+		| Hd(e1) -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
+						let newType = newVar count in
+							let newConstraint = [(typeE1, TyList(TyId(newType)))] in
+								(TyId(newType), List.concat[newConstraint; constraintE1])
+		| Tl(e1) -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
+						let newType = newVar count in
+							let newConstraint = [(typeE1, TyList(TyId(newType)))] in
+								(TyId(newType), List.concat[newConstraint; constraintE1])
+		| IsEmpty(e1) -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
+						let newType = newVar count in
+							let newConstraint = [(typeE1, TyList(TyId(newType)))] in
+								(TyBool, List.concat[newConstraint; constraintE1])
+		| Raise -> let newType = newVar count in (TyId(newType), [])
 		| Try(e1, e2) -> let (typeE1, constraintE1) = get_constraints envmnt e1 in
 							let (typeE2, constraintE2) = get_constraints envmnt e2 in
 								let newConstraint = [(typeE1, typeE2)] in
